fc152d9722398fa55d4b5fe7bd4ff5aa5e8f1cff
wicaks0 <195410103.nugroho.wicakso@gmail.com>
2021-10-15 20:42:20 +0700

    praktikum5

---
+++ DiceRollee/app/src/main/java/id/nw/dicerolle/MainActivity.kt
@@ -0,0 +1,38 @@
+package id.nw.dicerolle
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+            val resultTextView: TextView = findViewById(R.id.TextView)
+            resultTextView.text = "6"
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.TextView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
